3bf633eae48c8d3dd7d91824c2feb0c1764e7a78
Script Raccoon <scriptraccoon@gmail.com>
2023-03-17 00:49:06 +0100

    remove logs

--- src/routes/search/[name]/+page.server.ts
+++ src/routes/search/[name]/+page.server.ts
@@ -5,12 +5,9 @@ import { error } from "@sveltejs/kit";
 
 export const load: PageServerLoad = async (event) => {
 	const { name } = event.params;
-	console.log({ name });
 	const actor_id = await get_actor_id(name);
 	if (!actor_id) throw error(400, "No actor found");
-	console.log({ actor_id });
 	const movies = await get_movies(actor_id);
 	if (!movies) throw error(400, "No movies found");
-	console.log({ movies });
 	return { movies };
 };
